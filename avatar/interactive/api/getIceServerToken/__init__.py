132732b566b032ed231c9c5207756d5083025d27
jueun.hwang <jueunhwang@microsoft.com>
2024-07-29 16:07:07 -0500

    update api src

--- avatar/interactive/api/getIceServerToken/__init__.py
+++ avatar/interactive/api/getIceServerToken/__init__.py
@@ -9,19 +9,18 @@ import azure.functions as func
 subscription_key = os.getenv("AZURE_SPEECH_API_KEY")
 region = os.getenv("AZURE_SPEECH_REGION")
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # Define token endpoint
-    token_endpoint = "https://westeurope.api.cognitive.microsoft.com/cognitiveservices/avatar/relay/token/v1"
+    token_endpoint = f"https://{region}.tts.speech.microsoft.com/cognitiveservices/avatar/relay/token/v1"
 
     # Make HTTP request with subscription key as header
     response = requests.get(token_endpoint, headers={"Ocp-Apim-Subscription-Key": subscription_key})
 
     if response.status_code == 200:
         return func.HttpResponse(
-            body=json.dumps(response.json()),
+            body= json.dumps(response.json()),
             status_code=200,
             headers={"Content-Type": "application/json"}
         )
